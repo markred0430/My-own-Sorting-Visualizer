7aa92fbc9755dcb628225cfae4f88bc846748780
markred0430 <redilamarkanthony30@gmail.com>
2023-02-12 18:59:18 +0800

    Setting up buttons.

--- My-own-Sorting-Algorithm-Visualizer.py
+++ My-own-Sorting-Algorithm-Visualizer.py
@@ -12,17 +12,28 @@ forStyling.configure('Frame3.TFrame', background = '#735F32')
 forStyling.configure('Frame4.TFrame', background = 'black')
 
 # ==========WIDGETS==========
+# Main
 mainFrame = ttk.Frame(root, width=250, height=250, style = 'mainFrame.TFrame')
 mainFrame.grid()
 
+# For Title
 TitleFrame = ttk.Frame(mainFrame, width=1300, height= 100, style='Frame2.TFrame' )
 TitleLabel = ttk.Label(TitleFrame, text= "        Mark's Sorting Algorithm Visualizer", background='gray', foreground='black', font=('Times', 50, 'bold italic'))
 TitleLabel.grid(padx=15, pady=15, row=0, column=0, columnspan= 4, sticky='WE')
 TitleFrame.grid(padx=15, pady=15, row=0, column=0, columnspan= 4, sticky='WE')
 
+#For UI
 buttonFrame = ttk.Frame(mainFrame, width= 300, height= 400, style='Frame3.TFrame' )
+generate = tk.Button(buttonFrame, text="Generate", width=20, font="Arial", background='white', highlightcolor='red')
+start = tk.Button(buttonFrame, text="Start", width=20, font="Arial", background='white', highlightcolor='red')
+exit = tk.Button(buttonFrame, text="Exit", width=20, font="Arial", background='white', highlightcolor='red')
+
+generate.grid(padx=15, pady=15, row=0, column=0, sticky='WE')
+start.grid(padx=15, pady=15, row=1, column=0, sticky='WE')
+exit.grid(padx=15, pady=15, row=2, column=0, sticky='WE')
 buttonFrame.grid(padx=15, pady=15, row=1, column=0, sticky='WE')
 
+#For Simulation
 simulationFrame = ttk.Frame(mainFrame, width=980, height=400, style='Frame4.TFrame' )
 simulationFrame.grid(padx=15, pady=15, row=1, column=1)
 
@@ -32,4 +43,6 @@ simulationFrame.grid(padx=15, pady=15, row=1, column=1)
 
 root.columnconfigure(0, weight=2)
 root.rowconfigure(0, weight=2)
+
+
 root.mainloop()
