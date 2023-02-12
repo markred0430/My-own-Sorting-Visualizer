2bc3872884fc701f9ab52b3700614e7b6c3027c2
markred0430 <redilamarkanthony30@gmail.com>
2023-02-12 19:53:44 +0800

    creating List and Slider.

--- My-own-Sorting-Algorithm-Visualizer.py
+++ My-own-Sorting-Algorithm-Visualizer.py
@@ -1,6 +1,19 @@
 # Making a window using tkinter.
 import tkinter as tk
 from tkinter import ttk
+from tkinter import HORIZONTAL
+
+
+OPTIONS = [
+    "Selection Sort",
+    "Bubble Sort",
+    "Insertion Sort",
+    "Comb Sort",
+    "Cocktail Shaker Sort",
+    "Heap Sort",
+    "Quick Sort",
+    "Bogo Sort"
+]
 
 root = tk.Tk()
 
@@ -24,13 +37,25 @@ TitleFrame.grid(padx=15, pady=15, row=0, column=0, columnspan= 4, sticky='WE')
 
 #For UI
 buttonFrame = ttk.Frame(mainFrame, width= 300, height= 400, style='Frame3.TFrame' )
+variable = tk.StringVar(buttonFrame)
+variable.set(OPTIONS[0])  # default value
+dropDown = tk.OptionMenu(buttonFrame, variable, *OPTIONS)
+scale = tk.Scale(buttonFrame, from_=2, to=600 // 10, orient=HORIZONTAL, length=170, label="Size Bar")
+scale.set(10)
+scale_speed = tk.Scale(buttonFrame, from_=2, to=500, orient=HORIZONTAL, length=170, label="Speed")
+scale_speed.set(10)
 generate = tk.Button(buttonFrame, text="Generate", width=20, font="Arial", background='white', highlightcolor='red')
 start = tk.Button(buttonFrame, text="Start", width=20, font="Arial", background='white', highlightcolor='red')
 exit = tk.Button(buttonFrame, text="Exit", width=20, font="Arial", background='white', highlightcolor='red')
 
-generate.grid(padx=15, pady=15, row=0, column=0, sticky='WE')
-start.grid(padx=15, pady=15, row=1, column=0, sticky='WE')
-exit.grid(padx=15, pady=15, row=2, column=0, sticky='WE')
+dropDown.grid(padx=15, pady=15, row=0, column=0, sticky='WE')
+scale.grid(padx=15, pady=15, row=1, column=0, sticky='WE')
+scale_speed.grid(padx=15, pady=15, row=2, column=0, sticky='WE')
+generate.grid(padx=15, pady=15, row=3, column=0, sticky='WE')
+start.grid(padx=15, pady=15, row=4, column=0, sticky='WE')
+exit.grid(padx=15, pady=15, row=5, column=0, sticky='WE')
+
+
 buttonFrame.grid(padx=15, pady=15, row=1, column=0, sticky='WE')
 
 #For Simulation
